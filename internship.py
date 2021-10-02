d2e99bcfe6ee1b9f79e19bb7005a2e9a781685c7
akshatrshah <66900011+akshatrshah@users.noreply.github.com>
2021-10-02 16:22:07 +0530

    Update internship.py

--- internship.py
+++ internship.py
@@ -1,6 +1,17 @@
+#INTERNSHIP ASSIGNMENT FOR CONVIN
+
+#PROCEDURE
+#A general list is created which is used to store all the lines of the document, 
+# and a list question_tags is created which has the key words to determine if a 
+# statement is a question or not. We have to consider each statement,i.e., each element 
+# of the general list with the question_tags. If th e question tag is spotted anywhere 
+# in the statement a YES keyword is attached to it and send to result.txt. If the statement 
+# is not a question then a NO keyword is attached to the statement and sent to the result.txt.
+
+
 all_lines=[]
 #Taking file name input to read through and opening the files.
-file_name=input("Enter file name you want to test(.txt): ")
+file_name=input("Enter file name you want to test(without .txt extension): ")
 with open(file_name+".txt", "r") as f:
     with open("result.txt", "w+") as k: 
         #converting each line of the file to a list, where each line is represented as a element of the list.      
@@ -15,10 +26,10 @@ with open(file_name+".txt", "r") as f:
             for j in question_tags:
                 #when a question statement is found adding it to the result file with a YES.
                 if j in i:
-                    temp=i+" "*(50-len(i))+"Yes"
+                    temp=i+" "*(70-len(i))+"Yes"
                     k.write(f'{temp}\n')
                     break
             #when a question statement is not found and adding it to the result file with a NO.
             else:
-                temp1=i+" "*(50-len(i))+"No"
+                temp1=i+" "*(70-len(i))+"No"
                 k.write(f'{temp1}\n')
